989f61b583ca2724f638228bc93c480f9e635d02
Oszaki <78315391+Osamazaki@users.noreply.github.com>
2021-03-27 00:02:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-import datetime
 import pandas as pd
 from selenium.webdriver.common import keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,7 +9,8 @@ import time
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
-site = driver.get("https://www.youtube.com/")
+url = "https://www.youtube.com"
+site = driver.get(url)
 def channel_selector(channel_name, scroll_down_timer):
     timer = scroll_down_timer # in order to be able to catch as many results as you would want, decide it depending on the amount of content on the channel
     WebDriverWait(driver, 20).\
@@ -21,6 +21,7 @@ def channel_selector(channel_name, scroll_down_timer):
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="icon-label"]'))).click()
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[1]/div[2]/ytd-channel-sub-menu-renderer/div[2]/yt-sort-filter-sub-menu-renderer/yt-dropdown-menu/tp-yt-paper-menu-button/tp-yt-iron-dropdown/div/div/tp-yt-paper-listbox/a[1]/paper-item/tp-yt-paper-item-body/div[1]'))).click()
     while int(timer) > 0:
+        # scroll down to make elements visible and catchable
         driver.find_element_by_tag_name('body').send_keys(keys.Keys.END)
         timer -= 1
         time.sleep(2)
@@ -32,11 +33,16 @@ def parser(videos_elements):
     videos = []
     for video_element in videos_elements:
         video = {
-            "title": video_element.find_element_by_xpath('.//*[@id="video-title"]').text,  # must add a dot so that it searches inside the element
+            "title": video_element.find_element_by_xpath('.//*[@id="video-title"]').text,  # must add a dot so that it searches inside the element, to make it relative to the element, cuz of the //
             "view count": video_element.find_element_by_xpath('.//*[@id="metadata-line"]/span[1]').text,
-            "uploaded since": video_element.find_element_by_xpath('.//*[@id="metadata-line"]/span[2]').text
+            "uploaded since": video_element.find_element_by_xpath('.//*[@id="metadata-line"]/span[2]').text,
         }
+        if video_element.find_element_by_xpath('.//*[@id="video-title"]').get_attribute("href")[:1] == "/":
+            video["url"] = url + video_element.find_element_by_xpath('.//*[@id="video-title"]').get_attribute("href")
+        else :
+            video["url"] = video_element.find_element_by_xpath('.//*[@id="video-title"]').get_attribute("href")
         videos.append(video)
+
     return videos
 
 # using the Travellight channel for an example, and a scroll time of 5 secs
@@ -45,6 +51,7 @@ def parser(videos_elements):
 videos_items = channel_selector('Travellight', 5)
 parsed_data = parser(videos_items)
 df = pd.DataFrame(parsed_data)
+#df.to_excel("videos.xlsx")
 df.to_csv("videos")
 driver.quit()
 
